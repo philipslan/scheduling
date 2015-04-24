1a6eb71b675cac8dd63b21d2c723cfc2f2840c88
philipslan <philiplan2017@u.northwestern.edu>
2015-04-24 16:50:06 -0500

    Got Dates to work correctly yay javascript

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
 $( document ).ready(function(){
   arraymon=[];
   makeMonths();
+  selectMonths();
+});
+
+// Data
+var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+var fullMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var day = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
+var date = new Date();
+
+// Month Methods
+function selectMonths(){
   $(".mon").click(function(){
+
     a= $(this).html();
     one= new Date(date.getFullYear(), getMonthIndex(a),1);
     if ($(this).css('background-color') == "rgb(55, 253, 252)"){
       $(this).css('background-color','white');
-      removeMonth(one.getMonth());
-      hideWeeks(one.getMonth());
-      console.log(arraymon);
-      if (arraymon.length==0){
-        $(".weekselector").fadeOut();
-      }
+      removeMonth(one.getMonth()); 
+      remove= fullMonth[one.getMonth()];
+      $("#"+remove).remove();
     }
     else{
       $(this).css('background', '#37FDFC');
       arraymon.push(one);
       createWeek(one);
-      console.log(arraymon);
-      $(".weekselector").fadeIn();
     }
   });
-});
-
-var date = new Date();
-
+}
 function makeMonths(){
   var monthNum= date.getMonth();
   var i= monthNum;
   var j= 0;
   do {
-    var n= month[i];
     var html= ".month" + j;
-    $(html).html(n);
+    $(html).html(month[i]);
     i= (i+1)%12;
     j++;
   } while(i != monthNum)  
@@ -54,44 +57,41 @@ function getMonthIndex(fmonth){
   }
 }
 
+// Week Method
 function createWeek(firstday){
   weeks=[];
+  a= firstday.getMonth(); 
   if(firstday.getDay() != 0){
-    a= firstday.getMonth();
     week= new Date(firstday.getFullYear(), firstday.getMonth(),8-firstday.getDay());
     week1 = new Date(firstday.getFullYear(), firstday.getMonth(), week.getDate()+6);
-    date1= ""+(a+1)+"/"+week.getDate();
-    date2= ""+(a+1)+"/"+week1.getDate();
-    weeks.push([date1,date2]);
-    createWeekTemplate(fullMonth[a],weeks);
-    // do {
-    //   week= new Date(firstday.getFullYear(), firstday.getMonth(),8-firstday.getDay());
-    //   week1 = new Date(firstday.getFullYear(), firstday.getMonth, week.getDate()+6);
-    // }while(week.getMonth()==a)
-      
-    //   $(".weeks").append( "<br/>"+week1);
-    //   month[week1.getMonth()];
   }
+  else{
+    week= firstday;
+    week1 = new Date(firstday.getFullYear(), firstday.getMonth(), week.getDate()+6);
+  }
+  date1= ""+(a+1)+"/"+week.getDate();
+  date2= ""+(a+1)+"/"+week1.getDate();
+  weeks.push([date1,date2]);
+  do {
+    week= new Date(week.getFullYear(), week.getMonth(),week.getDate()+7);
+    week1 = new Date(week1.getFullYear(), week1.getMonth(), week1.getDate()+7);
+    if(week.getMonth()!=a){
+      break;
+    }      
+    date1= ""+(week.getMonth()+1)+"/"+week.getDate();
+    date2= ""+(week1.getMonth()+1)+"/"+week1.getDate();
+    weeks.push([date1,date2]);
+  }while(week.getMonth()==a)
+  createWeekTemplate(fullMonth[a],weeks);
 }
 
 function createWeekTemplate(month,weeks){
-  $(".weektemplate").append("<div class='row'><h3>"+month+"</h3></div><div id='"+month+"'class='row weeks'></div>");
-  template= ".weektemplate #"+month;
+  $(".weektemplate").append("<div id='"+month+"' ><div class='row'><h3>"+month+"</h3></div><div class='row weeks'></div></div>");
+  template= ".weektemplate #"+month+" .weeks";
   for(i=0; i<weeks.length; i++){
-    $(template).append("<div class='col-md-1'><h4>"+weeks[i][0]+"</h4><h4>"+weeks[i][1]+"</h4></div>");
+    $(template).append("<div class='col-md-1'><h4 class='week1'>"+weeks[i][0]+"</h4><h4 class='week2'>"+weeks[i][1]+"</h4></div>");
   }
-  //$(template).data("size",i);
+  $(template).append("<div class='col-md-1 select-all'><h4>Select</h4><h4>All</h4></div>");
 }
 
-function hideWeeks(mon){
-  mon= fullMonth[month.indexOf(mon)]
-  var a= "#"+mon;
-  $(a).fadeOut();
-  console.log($(a).css("display"));
-}
-
-// Datastructs
-var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
-var fullMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var day = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
 
