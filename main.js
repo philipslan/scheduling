b6a09c8d54c80687fb426583c5ea3a196c0bc9a4
philipslan <philiplan2017@u.northwestern.edu>
2015-04-24 13:15:57 -0500

    lots of spaghetti code

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ $( document ).ready(function(){
     if ($(this).css('background-color') == "rgb(55, 253, 252)"){
       $(this).css('background-color','white');
       removeMonth(one.getMonth());
+      hideWeeks(one.getMonth());
       console.log(arraymon);
       if (arraymon.length==0){
         $(".weekselector").fadeOut();
@@ -54,23 +55,43 @@ function getMonthIndex(fmonth){
 }
 
 function createWeek(firstday){
+  weeks=[];
   if(firstday.getDay() != 0){
     a= firstday.getMonth();
     week= new Date(firstday.getFullYear(), firstday.getMonth(),8-firstday.getDay());
-    week1 = new Date(firstday.getFullYear(), firstday.getMonth, week.getDate()+6);
-    
-    do {
-      week= new Date(firstday.getFullYear(), firstday.getMonth(),8-firstday.getDay());
-      week1 = new Date(firstday.getFullYear(), firstday.getMonth, week.getDate()+6);
-    }while(week.getMonth()==a)
+    week1 = new Date(firstday.getFullYear(), firstday.getMonth(), week.getDate()+6);
+    date1= ""+(a+1)+"/"+week.getDate();
+    date2= ""+(a+1)+"/"+week1.getDate();
+    weeks.push([date1,date2]);
+    createWeekTemplate(fullMonth[a],weeks);
+    // do {
+    //   week= new Date(firstday.getFullYear(), firstday.getMonth(),8-firstday.getDay());
+    //   week1 = new Date(firstday.getFullYear(), firstday.getMonth, week.getDate()+6);
+    // }while(week.getMonth()==a)
       
-      $(".weeks").append( "<br/>"+week1);
-      month[week1.getMonth()];
-    
+    //   $(".weeks").append( "<br/>"+week1);
+    //   month[week1.getMonth()];
   }
+}
+
+function createWeekTemplate(month,weeks){
+  $(".weektemplate").append("<div class='row'><h3>"+month+"</h3></div><div id='"+month+"'class='row weeks'></div>");
+  template= ".weektemplate #"+month;
+  for(i=0; i<weeks.length; i++){
+    $(template).append("<div class='col-md-1'><h4>"+weeks[i][0]+"</h4><h4>"+weeks[i][1]+"</h4></div>");
+  }
+  //$(template).data("size",i);
+}
+
+function hideWeeks(mon){
+  mon= fullMonth[month.indexOf(mon)]
+  var a= "#"+mon;
+  $(a).fadeOut();
+  console.log($(a).css("display"));
 }
 
 // Datastructs
 var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+var fullMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 var day = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
 
