ab456124cd9c3b5ab760c7e6bd91e427e42219cd
philipslan <philiplan2017@u.northwestern.edu>
2015-04-28 13:20:42 -0500

    select all adds to arry

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov",
 var fullMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 var day = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
 var date = new Date();
-arrayweek=[];
-rankmonth=[];
-currentmonth= date.getMonth()+1;
+var arrayweek=[];
+var rankmonth=[];
+var currentmonth= date.getMonth()+1;
 for (i=0; i<12; i++){
   rankmonth.push(currentmonth);
   if (currentmonth<12){
@@ -25,12 +25,12 @@ for (i=0; i<12; i++){
 // Month Methods
 function selectMonths(){
   $(".mon").click(function(){
-    a= $(this).html();
-    one= new Date(date.getFullYear(), getMonthIndex(a),1);
+    var a= $(this).html();
+    var one= new Date(date.getFullYear(), getMonthIndex(a),1);
     if ($(this).css('background-color') == "rgb(55, 253, 252)"){
       $(this).css('background-color','white');
       removeMonth(one.getMonth()); 
-      remove= fullMonth[one.getMonth()];
+      var remove= fullMonth[one.getMonth()];
       $("#"+remove).remove();
     }
     else{
@@ -53,7 +53,7 @@ function makeMonths(){
 }
 
 function removeMonth(index){
-  for (i=0; i<arraymon.length; i++){
+  for (var i=0; i<arraymon.length; i++){
     if (arraymon[i].getMonth()==index){
       arraymon.splice(i,1);
     }
@@ -61,7 +61,7 @@ function removeMonth(index){
 }
 
 function getMonthIndex(fmonth){
-  for(i=0; i<month.length; i++){
+  for(var i=0; i<month.length; i++){
     if (month[i]==fmonth){
       return i;
     }
@@ -70,18 +70,18 @@ function getMonthIndex(fmonth){
 
 // Week Method
 function createWeek(firstday){
-  weeks=[];
-  a= firstday.getMonth(); 
+  var weeks=[];
+  var a= firstday.getMonth(); 
   if(firstday.getDay() != 0){
-    week= new Date(firstday.getFullYear(), firstday.getMonth(),8-firstday.getDay());
-    week1 = new Date(firstday.getFullYear(), firstday.getMonth(), week.getDate()+6);
+    var week= new Date(firstday.getFullYear(), firstday.getMonth(),8-firstday.getDay());
+    var week1 = new Date(firstday.getFullYear(), firstday.getMonth(), week.getDate()+6);
   }
   else{
-    week= firstday;
-    week1 = new Date(firstday.getFullYear(), firstday.getMonth(), week.getDate()+6);
+    var week= firstday;
+    var week1 = new Date(firstday.getFullYear(), firstday.getMonth(), week.getDate()+6);
   }
-  date1= (a+1)+"/"+week.getDate();
-  date2= (a+1)+"/"+week1.getDate();
+  var date1= (a+1)+"/"+week.getDate();
+  var date2= (a+1)+"/"+week1.getDate();
   weeks.push([date1,date2]);
   do {
     week= new Date(week.getFullYear(), week.getMonth(),week.getDate()+7);
@@ -98,8 +98,8 @@ function createWeek(firstday){
 
 function createWeekTemplate(month,weeks){
   $(".weektemplate").append("<div id='"+month+"' ><div class='row'><h3>"+month+"</h3></div><div class='row weeks'></div></div>");
-  template= ".weektemplate #"+month+" .weeks";
-  for(i=0; i<weeks.length; i++){
+  var template= ".weektemplate #"+month+" .weeks";
+  for(var i=0; i<weeks.length; i++){
     $(template).append("<div class='week col-md-1 "+month+"'><h4 class='week1'>"+weeks[i][0]+"</h4><h4 class='week2'>"+weeks[i][1]+"</h4></div>");
   }
   $(template).append("<div class='col-md-1 select-all "+month+"'><h4>Select</h4><h4>All</h4></div>");
@@ -107,53 +107,62 @@ function createWeekTemplate(month,weeks){
 
 // Day Methods
 $(document).on("click",".week",function(){
-    if ($(this).css('background-color') == "rgb(55, 253, 252)"){
-      $(this).css('background-color','white');
-      // removeMonth(one.getMonth()); 
-      // remove= fullMonth[one.getMonth()];
-      // $("#"+remove).remove();
-    }
-    else{
-      $(this).css('background', '#37FDFC');
-      week1= $(this).find(".week1").html();
-      week2= $(this).find(".week2").html();
-
-      week1= week1.split("/");
-      week1= week1.map(function(x){
-        return parseInt(x,10); 
-      });
-      
-      week2= week2.split("/");
-      week2= week2.map(function(x){
-        return parseInt(x,10);
-      });
-      result= [week1,week2];
-      pushweek(result);
-      console.log(arrayweek);
-    }
-  });
+  var result= findWeeks($(this));
+  // Deselect
+  if ($(this).css('background-color') == "rgb(55, 253, 252)"){
+    $(this).css('background-color','white');
+    removeWeek(result);
+    console.log("Dates");
+    for (var i=0; i<arrayweek.length; i++){ console.log(arrayweek[i][0]);} // prints each date
+  } // Select
+  else{
+    $(this).css('background', '#37FDFC');
+    pushWeek(result);
+    console.log("Dates");
+    for (var i=0; i<arrayweek.length; i++){ console.log(arrayweek[i][0]);} // prints each date
+  }
+});
 
 $(document).on("click",".select-all",function(){
-  a= $(this).parent();
-  length= a[0].children.length;
-  selectall= a[0].children[length-1];
-  console.log(a);
+  var a= $(this).parent();
+  var length= a[0].children.length;
+  var selectall= a[0].children[length-1];
   if ($(selectall).css('background-color') == "rgb(55, 253, 252)"){
-    for (i=0; i< length; i++){
-      selected= a[0].children[i];
+    for (var i=0; i< length; i++){
+      var selected= a[0].children[i];
       $(selected).css('background-color','white');
+      console.log(selected);
     }
   }
   else{
-    for(i=0; i<length; i++){
-      selected= a[0].children[i];
+    for(var k=0; k<length; k++){
+      var selected= a[0].children[k];
       $(selected).css('background', '#37FDFC');
-    }
-  }
-  console.log($(this).css('background-color'));
+      if(k< length -1){
+        var result= findWeeks(selected);
+        if(arrayweek.length==0){
+          pushWeek(result);
+        }
+        else{
+          found= false;
+          for(var j=0; j<arrayweek.length; j++){
+            if( (result[0][0]==arrayweek[j][0][0]) && (result[0][1]==arrayweek[j][0][1]) ){
+              found= true;
+              break;
+            } // end if
+          } // end for
+          if (!found){
+            pushWeek(result);
+            console.log("Dates");
+            for (var i=0; i<arrayweek.length; i++){ console.log(arrayweek[i][0]);} // prints each date
+          }
+        } // end else
+      } // end if k< length -1
+    } // end for
+  } // end else
 });
 
-function pushweek(result){
+function pushWeek(result){
   if (arrayweek.length==0){
     arrayweek.push(result);
   }
@@ -162,13 +171,9 @@ function pushweek(result){
     for(i=0; i<arrayweek.length; i++){
       first= rankmonth.indexOf(arrayweek[i][0][0]);
       second= rankmonth.indexOf(result[0][0]);
-      if(first<second){
-        console.log("pass");
-      }
+      if(first<second){}
       else if(first==second){
-        if(arrayweek[i][0][1]<result[0][1]){
-          console.log("pass");
-        }
+        if(arrayweek[i][0][1]<result[0][1]){}
         else{
           arrayweek.splice(i,0,result);
           end= true;
@@ -187,4 +192,30 @@ function pushweek(result){
   }
 }
 
+function removeWeek(result){
+  a=result[0];
+  for (i=0; i<arrayweek.length; i++){
+    if(a[0]==arrayweek[i][0][0]){
+      if(a[1]==arrayweek[i][0][1]){
+        arrayweek.splice(i,1);
+      }
+    }    
+  }
+}
 
+function findWeeks(div){
+  week1= $(div).find(".week1").html();
+  week2= $(div).find(".week2").html();
+
+  week1= week1.split("/");
+  week1= week1.map(function(x){
+    return parseInt(x,10); 
+  });
+  
+  week2= week2.split("/");
+  week2= week2.map(function(x){
+    return parseInt(x,10);
+  });
+  result= [week1,week2];
+  return result;
+}
